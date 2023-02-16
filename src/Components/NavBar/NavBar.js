07c36e1ecaad4a693b8b91c421951b07b3d466bc
nithin <nithinmani2001@gmail.com>
2023-02-16 13:48:01 +0530

    second edit

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,14 +1,53 @@
-import React from 'react'
-import "./NavBar.css"
-
+import React from "react";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div className='navBar'>
-    <img className='logo' alt='logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCCUIaNqembdjiKSlDv0XjcCDnf0WB9jprlg&usqp=CAU "/>
-    <img className='avatar' alt='avatar' src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg&ga=GA1.2.1496000322.1670683814&semt=sph"/>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dde8ff">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">INED</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Tutorial</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Profile</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control mr-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
